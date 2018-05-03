3021f079525c358012675dffcc71da8a13fbd54c
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-05-03 17:43:06 +0300

    Initial

---
+++ README.rst
@@ -0,0 +1,14 @@
+Documentation
+=============
+
+OCDS API is project which build an API over the OCDS releases database.
+
+Building
+--------
+
+Use openprocurement.ocds.buildout 
+
+
+Specs
+----------
+`API specification <https://github.com/open-contracting/api-specification>`_
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = """
+OCDS DB Dump tool
+"""
+
+
+install_requires = [
+    'setuptools',
+    'gevent',
+    'apscheduler',
+    'ocdsapi'
+]
+
+test_requires = [
+    'pytest',
+    'pytest-cov'
+]
+
+extra = install_requires + test_requires
+
+entry_points = {
+    'release_pack': [
+        'releases = ocdsapi.outlet.run:Run',
+    ],
+    'ocdsapi.outlets': [
+        'filesystem = ocdsapi.outlet.backends.fs:FSOutlet'
+    ]
+}
+
+setup(name='ocdsapi.outlet',
+      version='0.1.5',
+      description=DESCRIPTION,
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      include_package_data=True,
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={"test": extra},
+      tests_require=test_requires,
+      entry_points=entry_points
+      )
